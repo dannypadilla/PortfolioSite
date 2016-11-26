afe708cd44d1e7c9cc7ced064fde5006b03688f3
dannypadilla <disavowed10@gmail.com>
2016-11-25 19:46:06 -0800

    test data

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,4 +3,10 @@ var myApp = angular.module("myApp", [] );
 
 myApp.controller("AppCtrl", ["$scope", "$http", function($scope, $http) {
     console.log("Helloz");
+
+    $http.get("/contactList").success(function(response) {
+	console.log("I got the data I requested");
+	$scope.contactList = response;
+    });
+
 }]);
--- server.js
+++ server.js
@@ -3,5 +3,32 @@ var app = express();
 
 app.use(express.static(__dirname + "/public") );
 
+app.get("/contactList", function(req, res) {
+    console.log("Got a get request");
+
+    person1 = {
+	name: "Eric",
+	email: "cartman@sp.com",
+	number: "666-666-6666"
+    };
+
+    person2 = {
+	name: "Danny",
+	email: "p@dilla.com",
+	number: "111-111-1111"
+    };
+
+    person3 = {
+	name: "turd",
+	email: "poo@pee.com",
+	number: "222-222-2222"
+    };
+
+    var contactList = [person1, person2, person3];
+
+    res.json(contactList);
+
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
