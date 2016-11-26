44d716235b2da1ce9786060c11b44ec076c1ab68
dannypadilla <disavowed10@gmail.com>
2016-11-25 19:59:21 -0800

    modify server.js to handle mongojs GET requests

--- server.js
+++ server.js
@@ -1,32 +1,17 @@
 var express = require("express");
 var app = express();
+var mongojs = require("mongojs");
+var db = mongojs("contactList", ["contactList"] );
 
 app.use(express.static(__dirname + "/public") );
 
 app.get("/contactList", function(req, res) {
-    console.log("Got a get request");
+    console.log("Received a GET request");
 
-    person1 = {
-	name: "Eric",
-	email: "cartman@sp.com",
-	number: "666-666-6666"
-    };
-
-    person2 = {
-	name: "Danny",
-	email: "p@dilla.com",
-	number: "111-111-1111"
-    };
-
-    person3 = {
-	name: "turd",
-	email: "poo@pee.com",
-	number: "222-222-2222"
-    };
-
-    var contactList = [person1, person2, person3];
-
-    res.json(contactList);
+    db.contactList.find(function(err, docs) {
+	 console.log(docs);
+	res.json(docs);
+    });
 
 });
 
